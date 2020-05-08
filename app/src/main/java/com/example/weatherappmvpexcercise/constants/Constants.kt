07c3135a3bb244918e19a65a6990b3c9d266c119
sagitozz <sagitopwnz@gmail.com>
2020-05-08 14:22:36 +0300

    added weather icons

--- app/src/main/java/com/example/weatherappmvpexcercise/constants/Constants.kt
+++ app/src/main/java/com/example/weatherappmvpexcercise/constants/Constants.kt
@@ -2,7 +2,7 @@ package com.example.weatherappmvpexcercise.constants
 
 object Constants {
 
-    const val WEATHER_API_KEY = "76afd6b491msh5667831f19ac74dp15fa6bjsn58b7c52a950c"
+    const val WEATHER_API_KEY = "b7703ee94cmshba95e81137c2d80p1c02e3jsn50e532ae049f"
     const val WEATHER_BASE_URL = "https://weatherbit-v1-mashape.p.rapidapi.com"
     const val COORDINATES_BASE_URL = "http://api.ipstack.com/"
     const val LOG_TAG = "MyLog"
